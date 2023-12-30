7715f2778622e5702ef6713128bc0f377599e77f
Anukul Pandey <anukul.030601@gmail.com>
2023-12-30 15:06:30 +0530

    for loop

--- main.go
+++ main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	var a = math.Sin(math.Pi / 2)
-	fmt.Println(a)
+	// basic loop
+	i := 0
+	for i < 3 {
+		fmt.Println(i)
+		i++
+	}
+	// conventional for loop
+	for j := 0; j < 3; j += 1 {
+		fmt.Println(j)
+	}
+	// for without condition
+	for {
+		fmt.Println("anukul")
+		break
+	}
+	// continue
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue //skips the next code blocks
+		}
+		fmt.Println(n)
+	}
 }
